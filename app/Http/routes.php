56940d74d5f91797357356da8f06c03868f3d315
Jessica Legner <jessicalegner@Jessicas-MacBook-Air.local>
2016-04-20 01:01:15 -0400

    mght not need to have this slash

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,7 +8,7 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('/dashboard', array('as' => 'dashboard', 'uses' => 'PagesController@dashboard'));
     Route::get('/tickets/new', array('as' => 'tickets.new', 'uses' => 'PagesController@newTickets'));
 
-    Route::get('/tickets', array('as' => 'tickets.index', 'uses' => 'TicketController@index'));
+    Route::get('tickets', array('as' => 'tickets.index', 'uses' => 'TicketController@index'));
 	Route::resource('ticket', 'TicketController', ['except' => ['index']]);
 	Route::get('/ticket/customer/{customer}', array('as'=> 'ticket.createForCustomer', 'uses' => 'TicketController@createForCustomer'));
 	Route::get('/ticket/{ticket}/print', array('as'=> 'ticket.print', 'uses' => 'TicketController@printTicket'));
