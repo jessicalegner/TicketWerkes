a9ca3a3a156c767b954a1c2b52e562a3c053f4a8
Jessica Legner <jessicalegner@Jessicas-MacBook-Air.local>
2016-06-20 22:04:00 -0400

    minor fixes

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -78,7 +78,7 @@ $(document).ready(function(){
 
     $('button[data-confirm]').click(function(ev) {
         if (!$('#dataConfirmModal').length) {
-            $('body').append('<div class="modal fade" tabindex="-1" role="dialog" id="dataConfirmModal"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">Delete Customer</h4></div><div class="modal-body"><p>One fine body&hellip;</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button><button id="dataConfirmOK" type="button" class="btn btn-danger">Delete</button></div></div><!-- /.modal-content --></div><!-- /.modal-dialog --></div><!-- /.modal -->');
+            $('body').append('<div class="modal fade" tabindex="-1" role="dialog" id="dataConfirmModal"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">Delete Customer</h4></div><div class="modal-body"><p>One fine body&hellip;</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">No, Take Me Back to Safety</button><button id="dataConfirmOK" type="button" class="btn btn-danger">Delete</button></div></div><!-- /.modal-content --></div><!-- /.modal-dialog --></div><!-- /.modal -->');
         } 
         $('#dataConfirmModal').find('.modal-body').text($(this).attr('data-confirm'));
         $('#dataConfirmOK').click(function() {
